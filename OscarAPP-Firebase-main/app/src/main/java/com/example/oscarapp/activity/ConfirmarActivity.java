81fd0e39f94fc26083e91238d8300cd8ef96a5ad
Daffyne Alves Dias <daffynead@baritecnologia.com.br>
2021-03-08 22:38:06 -0300

    Foii

---
+++ OscarAPP-Firebase-main/app/src/main/java/com/example/oscarapp/activity/ConfirmarActivity.java
@@ -0,0 +1,49 @@
+package com.example.oscarapp.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.oscarapp.R;
+
+import static com.example.oscarapp.activity.DetalheFilme.salvarFilme;
+
+public class ConfirmarActivity extends AppCompatActivity {
+
+    TextView Diretor, Filme;
+
+    public String nomediretor;
+    //public String nomeFilme;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_confirmar);
+
+        Diretor = (TextView) findViewById(R.id.txtDiretor);
+        Filme = (TextView) findViewById(R.id.txtFilme);
+
+        Intent intentR = getIntent();
+        Bundle paramR = intentR.getExtras();
+
+
+        if(paramR != null ){
+            String nomeDi = paramR.getString("nome");
+            int idDi = paramR.getInt("id");
+                nomediretor = nomeDi;
+                Diretor.setText(nomeDi);
+            if (paramR.getString("nomeFilme") != null) {
+                Filme.setText(salvarFilme.getNome());
+            }
+        }else if(salvarFilme.getNome() != null){
+            Filme.setText(salvarFilme.getNome());
+        }
+    }
+
+
+
+
+
+}
